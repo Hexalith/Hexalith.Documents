d9c91933da0d23c70f7b1a93d4e5be2dbe7d4448
Jérôme Piquot <jpiquot@itaneo.com>
2024-12-06 21:11:01 +0100

    Update subproject, add handlers, and refactor projection
    Updated subproject commit hash and marked as dirty.
    Added new namespace `Hexalith.Documents.UI.Services.FileTypes.Projections.Details`.
    Introduced three new projection handler classes for specific events.
    Refactored `ActorProjectionFactory<TState>`: removed public `GetProjectionActor`, added `RemoveStateAsync`, and made `GetProjectionActor` private.

---
+++ src/Presentation/Hexalith.Documents.UI.Services/FileTypes/Projections/Details/FileTypeTargetRemovedOnDetailsProjectionHandler.cs
@@ -0,0 +1,32 @@
+﻿namespace Hexalith.Documents.UI.Services.FileTypes.Projections.Details;
+
+using System.Diagnostics.CodeAnalysis;
+
+using Hexalith.Application.Projections;
+using Hexalith.Documents.Events.FileTypes;
+using Hexalith.Documents.UI.Services.FileTypes.ViewModels;
+
+/// <summary>
+/// Handles the projection update when a FileTypeTargetRemoved event is received.
+/// </summary>
+/// <remarks>
+/// Initializes a new instance of the <see cref="FileTypeTargetRemovedOnDetailsProjectionHandler"/> class.
+/// </remarks>
+/// <param name="factory">The projection factory.</param>
+public class FileTypeTargetRemovedOnDetailsProjectionHandler(IProjectionFactory<FileTypeDetailsViewModel> factory) : FileTypeDetailsProjectionHandler<FileTypeTargetRemoved>(factory)
+{
+    /// <inheritdoc/>
+    protected override Task<FileTypeDetailsViewModel?> ApplyEventAsync([NotNull] FileTypeTargetRemoved baseEvent, FileTypeDetailsViewModel? model, CancellationToken cancellationToken)
+    {
+        ArgumentNullException.ThrowIfNull(baseEvent);
+        if (model == null)
+        {
+            return Task.FromResult<FileTypeDetailsViewModel?>(null);
+        }
+
+        return Task.FromResult<FileTypeDetailsViewModel?>(model with
+        {
+            Targets = model.Targets.Where(p => p != baseEvent.Target),
+        });
+    }
+}
