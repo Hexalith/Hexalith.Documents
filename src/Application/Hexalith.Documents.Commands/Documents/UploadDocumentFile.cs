50865fd0ad30aa82191290426d33f7035dc90389
Jérôme Piquot <jpiquot@itaneo.com>
2025-03-11 15:39:10 +0100

    Add document handling features and update data classes
    - Updated `ConstructionSiteQuickStartData` to include a new `DocumentData` property for predefined documents and added `Document2Id` for the second document's ID.
    - Introduced `AddDocumentFileHandler` class in `AddDocumentFileHandler.cs` to manage export requests, including methods for processing document storage and writing results to files.
    - Modified `UploadDocumentFile.cs` to define a new command record with properties for document details, supporting polymorphic serialization.

---
+++ src/Application/Hexalith.Documents.Commands/Documents/UploadDocumentFile.cs
@@ -0,0 +1,23 @@
+﻿namespace Hexalith.Documents.Commands.Documents;
+
+using System.Runtime.Serialization;
+
+using Hexalith.PolymorphicSerialization;
+
+/// <summary>
+/// Command to create a new document.
+/// </summary>
+/// <param name="Id">The unique identifier of the document.</param>
+/// <param name="FileTypeId">The type of the document.</param>
+/// <param name="Name">The name of the document.</param>
+/// <param name="Content">The content of the document in base64.</param>
+[PolymorphicSerialization]
+public partial record UploadDocumentFile(
+    string Id,
+    [property: DataMember(Order = 2)] string FileTypeId,
+    [property: DataMember(Order = 3)] string Name,
+    [property: DataMember(Order = 2)] string ContentType,
+    [property: DataMember(Order = 4)] string Content)
+    : DocumentCommand(Id)
+{
+}
